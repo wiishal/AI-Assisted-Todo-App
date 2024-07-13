b9a818498181e06f83ac7719d014a3b3b3bcba20
vishal-Nanaware <vishalnanaware7850@gmail.com>
2024-07-13 23:25:32 +0530

    added athentication

---
+++ src/auth/Login.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import axios from "axios"
+
+function Login({ variable, handle }) {
+  const [username, setUsername] = useState("")
+  const [password, setpassword] = useState("");
+  console.log(variable);
+
+  function handleUsename(e){
+    setUsername(e.target.value)
+  }
+  function handlePassword(e) {
+    setpassword(e.target.value);
+  }
+  function submit(){
+    console.log("in submit", username)
+    axios
+      .post("http://localhost:3001/auth/", {
+        username: username,
+        password: password,
+      })
+      .then(function (response) {
+        console.log(response.data.user);
+        handle(true, response.data.user);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+ 
+  return (
+    <div>
+    <h3>login</h3>
+      <div className="username-div">
+        <label htmlFor="">Username</label>
+        <input 
+        value={username}
+        onChange={handleUsename}
+        type="text" />
+      </div>
+      <div className="password-Div">
+        <label htmlFor="">Password</label>
+        <input
+        value={password}
+        onChange={handlePassword}
+        type="text" />
+      </div>
+      <div className="submit-btn">
+    <button onClick={submit}>submit</button>
+    <button> forgetPassword</button>
+
+      </div>
+    </div>
+  );
+}
+
+export default Login
