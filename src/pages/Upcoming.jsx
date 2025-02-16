d05b0ca8e5923e62c43b27ee5dbb8fdd04da6ba3
vishal-Nanaware <vishalnanaware7850@gmail.com>
2025-02-16 21:43:11 +0530

    modified Upcoming page

--- src/pages/Upcoming.jsx
+++ src/pages/Upcoming.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "../style/Upcoming.css";
+import { Await } from "react-router";
+import { getAllTasks } from "../services/taskService";
+import { useFormatDate } from "../hooks/useFormateDate";
 
 function Upcoming({ navCount }) {
+ const { formatDate, formatDateTomorrow } = useFormatDate();
   const [Tasks, setTasks] = useState([]);
   const [TaskLength, setTaskLenght] = useState(0);
   const [groupedTasks, setGroupedTasks] = useState({
@@ -13,80 +17,40 @@ function Upcoming({ navCount }) {
   const [isdiplayTask, setIsDisplayTask] = useState(null);
 
   useEffect(() => {
-    fetchTask();
-  }, []);
-
-  const fetchTask = () => {
-    axios
-      .get("http://localhost:3000/api/")
-      .then((response) => {
-        console.log(response.data.array);
-        const grouped = groupTasksByDate(response.data.array);
-        setTasks(response.data.array);
-
-        console.log("grouped", grouped);
-        setGroupedTasks(grouped);
-        // console.log(groupedTasks.tomorrow.length);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  function displayTask(id) {
-    for (let i = 0; i < Tasks.length; i++) {
-      if (Tasks[i].taskId === id) {
-        setIsDisplayTask(i);
-        return;
-      }
+    if (formatDate && formatDateTomorrow) {
+      fetchTask();
     }
-  }
-  const formatDate = (date) => {
-    const day = String(date.getDate());
-    const month = String(date.getMonth() + 1); // Months are zero-indexed
-    const year = String(date.getFullYear()).slice(-2); // Get last 2 digits of the year
-    return `${day}-${month}-${year}`;
-  };
-  const getTodayDate = () => {
-    const today = new Date();
-    return formatDate(today);
-  };
+  }, [formatDateTomorrow]);
 
-  const getTomorrowDate = () => {
-    const tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate() + 1);
-    console.log(tomorrow.getDate() + 1, "from tomorrow", tomorrow);
-    return formatDate(tomorrow);
+  const fetchTask = async () => {
+      const response = await getAllTasks();
+      console.log(response.tasks);
+      if(response.tasks){
+        grouped(response.tasks);
+      }
+       
   };
-
-  const groupTasksByDate = (Tasks) => {
-    // console.log(Tasks.length, "from grp");
-    setTaskLenght(Tasks.length);
-    const today = getTodayDate();
-    const tomorrow = getTomorrowDate();
-    console.log(tomorrow, "for each");
-    const groupedTasks = {
+  function grouped(data){
+    const groupedObj = {
       today: [],
       tomorrow: [],
       other: [],
     };
-
-    Tasks.forEach((task) => {
-      if (task.date === today) {
-        groupedTasks.today.push(task);
-      } else if (task.date === tomorrow) {
-        groupedTasks.tomorrow.push(task);
+    console.log("tommow",formatDateTomorrow);
+    data.forEach(task => {
+      if (task.date == formatDate) {
+        groupedObj.today.push(task);
+      } else if (task.date == formatDateTomorrow) {
+        groupedObj.tomorrow.push(task);
       } else {
-        groupedTasks.other.push(task);
+        groupedObj.other.push(task);
       }
     });
-    navCount((prev) => ({
-      ...prev,
-      upcoming: groupedTasks.tomorrow.length,
-    }));
-    console.log(groupedTasks.tomorrow.length);
-    return groupedTasks;
-  };
+    console.log(groupedObj)
+    setGroupedTasks(groupedObj);
+  }
+  console.log(formatDate);
+
   return (
     <div className="Upcoming-main">
       <div className="Upcoming-main-title">
@@ -129,21 +93,21 @@ function Upcoming({ navCount }) {
 function TaskTemplate({ task }) {
   return (
     <div className="Upcoming-task" key={task.taskId}>
-      {task.status === false ? (
+      {task.complete === false ? (
         <img src="/assets/dry-clean.png" alt="" width={13} height={13} />
       ) : (
         <img src="/assets/check-mark.png" alt="" width={13} height={13} />
       )}
 
       <div className="Upcoming-task-subdiv">
-        {task.status === false ? (
-          <p className="Upcoming-task-title">{task.taskDescription}</p>
+        {task.complete === false ? (
+          <p className="Upcoming-task-title">{task.title}</p>
         ) : (
           <p
             className="Upcoming-task-title"
-            style={{ color: "rgb(217, 217, 217)" }} 
+            style={{ color: "rgb(217, 217, 217)" }}
           >
-            {task.taskDescription}
+            {task.title}
           </p>
         )}
 
