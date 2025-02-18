31cc908e8eaa2a9c0eb8249e7b353d04321bd730
vishal-Nanaware <vishalnanaware7850@gmail.com>
2025-02-18 13:35:18 +0530

    modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
 import "./App.css"
-import axios from "axios";
+import "../src/style/listDetail.css";
+import "../src/style/listDetail.css";
 import Nav from "./componant/Nav";
 import Today from "./pages/Today";
 import Upcoming from "./pages/Upcoming";
 import Calender from "./pages/Calender";
 import Expenses from "./pages/Expenses.jsx";
-import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
-import ListDetails from "./componant/ListDetails.jsx";
-import TagDetails from "./componant/TagDetails.jsx";
+import TagDetails from "./componant/tags/TagDetails.jsx";
+import ListDetails from "./componant/list/ListDetails.jsx";
 
 function App({ user }) {
-  const [count, setCount] = useState({
-    today: 0,
-    upcoming: 0,
-    Tags:[],
-  });
-  
+ 
   return (
     <BrowserRouter>
       <div className="main">
         <Nav currUser={user}  />
         <Routes>
-          <Route path="/" element={<Today navCount={setCount} />} />
-          <Route path="/Upcoming" element={<Upcoming navCount={setCount} />} />
+          <Route path="/" element={<Today  />} />
+          <Route path="/Upcoming" element={<Upcoming />} />
           <Route path="/Calender" element={<Calender />} />
           <Route path="/Expenses" element={<Expenses />} />
           <Route path="/Lists/:item" element={<ListDetails />} />
